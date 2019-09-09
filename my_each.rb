302340714a2acdeac5911c2f12e4af7d3961a1af
KG ZZ <czheng5h88@gmail.com>
2019-09-09 18:14:57 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
+def my_each(collection) # put argument(s) here
   # code here
+  counter = 0 
+  while counter < collection.length 
+    yield collection[counter]
+    counter += 1
+  end 
+  collection
 end
